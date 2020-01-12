a39eb9a0f4f1b46e40e2be90524e933a3f87e2cc
yese <1321810135@qq.com>
2020-01-12 21:01:02 +0800

    config分布式配置

---
+++ springcloud-config-server-8101/src/main/java/com/yese/Config_Server_8101_Application.java
@@ -0,0 +1,14 @@
+package com.yese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class Config_Server_8101_Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Config_Server_8101_Application.class, args);
+    }
+}
